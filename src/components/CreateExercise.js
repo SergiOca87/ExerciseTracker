f661a1465a4ddfacc46e1a561a09c2be83092f59
ocasergi@gmail.com <ocasergi@gmail.com>
2020-01-22 00:15:19 +0100

    state

--- src/components/CreateExercise.js
+++ src/components/CreateExercise.js
@@ -1,19 +1,46 @@
 import React from 'react';
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
 
 class CreateExercise extends React.Component {
 
     //This fields should be equal to the fields in our MongoDB
     state = {
-        usernam: '',
+        username: '',
         description: '',
         series: 0,
         date: new Date(),
         users: []
     }
 
+    //This data will come from the DB
+    componentDidMount = () => {
+        this.setState({
+            users: ['test user'],
+            username: 'test user'
+        })
+    };
+
+    onChangeUsername = (e) => {
+        this.setState({
+            username: e.target.value,
+        });
+    };
+
+    onFormSubmit = (e) => {
+        e.preventDefault();
+
+    }
+
     render() {
         return(
-            <h2>CreateExercise</h2>
+            <form onSubmit ={this.onFormSubmit}>
+                <select>
+                    { this.state.users.map((user) => {
+                        return <option value={user}>{user}</option>
+                    }) }
+                </select>
+            </form>
         )
     }
 };
