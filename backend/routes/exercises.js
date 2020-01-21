32b47335e908637abfed585b5753584f262dd2d0
ocasergi@gmail.com <ocasergi@gmail.com>
2020-01-21 19:50:22 +0100

    starting front

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -35,4 +35,37 @@ router.route('/add').post((req, res) => {
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
+router.route('/:id').get((req, res) => {
+
+    // findById is another Moongose method that allows us to get specific
+    // entires form the db. We get the id from the params in the URL
+    Exercise.findById(req.params.id)
+        .then((exercise) => res.json(exercise))
+        .catch((err) => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').delete((req, res) => {
+    Exercise.findByIdAndDelete(req.params.id)
+        .then(() => res.json('Exercise Deleted.'))
+        .catch((err) => res.status(400).json('Error: ' + err));
+});
+
+// In this route, we pass in a json Object and assign the fields to an exercise
+// which we find by ID, we assing new values to it.
+router.route('/update/:id').post((req, res) => {
+    Exercise.findById(req.params.id)
+        .then((exercise) => {
+            exercise.username = req.body.username;
+            exercise.description = req.body.description;
+            exercise.duration = Number(req.body.duration);
+            exercise.date = Date.parse(req.body.date);
+
+            exercise.save()
+                .then(() => res.json('Exercise Updated!'))
+                .catch((err) => res.status(400).json('Error: ' + err));
+        })
+
+        .catch((err) => res.status(400).json('Error: ' + err));
+});
+
 module.exports = router;
