34166f5cd47726f58a8c470d3408833d3bde375e
Sergi <ocasergi@gmail.com>
2020-01-22 09:46:23 +0100

    axios

--- src/components/CreateExercise.js
+++ src/components/CreateExercise.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
+import Layout from './Layout';
 
 class CreateExercise extends React.Component {
 
@@ -25,22 +26,85 @@ class CreateExercise extends React.Component {
         this.setState({
             username: e.target.value,
         });
+	};
+	
+	onChangeDescription = (e) => {
+        this.setState({
+            description: e.target.value,
+        });
+	};
+	
+	onChangeSeries = (e) => {
+        this.setState({
+            series: e.target.value,
+        });
+	};
+	
+	onChangeDate = (date) => {
+        this.setState({
+            date: date,
+        });
     };
 
     onFormSubmit = (e) => {
         e.preventDefault();
 
+		//Push to DB
+		const exercise = {
+            username: this.state.username,
+			description: this.state.description,
+			series: this.state.series,
+			date: this.state.date,
+			users: this.state.users,
+        }
     }
 
     render() {
         return(
-            <form onSubmit ={this.onFormSubmit}>
-                <select>
-                    { this.state.users.map((user) => {
-                        return <option value={user}>{user}</option>
-                    }) }
-                </select>
-            </form>
+			<Layout>
+				<h3>Add a New Exercise</h3>
+				<form onSubmit ={this.onFormSubmit}>
+					<div className="form-group">
+						<label for="userSelect">User</label>
+						<select 
+							id="userSelect"
+							value={this.state.userName}
+							onChange={this.onChangeUsername}>
+							{ 
+								this.state.users.map((user) => {
+									return <option key={user} value={user}>{user}</option>
+								}) 
+							}
+						</select>
+					</div>
+					<div className="form-group">
+						<label for="description">Description</label>
+						<input 
+							type="text"
+							className="form-control"
+							id="description"
+							value={this.state.description}
+							onChange={this.onChangeDescription} />
+					</div>
+					<div className="form-group">
+						<label for="series"># of Series</label>
+						<input 
+							type="number"
+							className="form-control"
+							id="series"
+							value={this.state.series}
+							onChange={this.onChangeSeries} />
+					</div>
+					<div className="form-group">
+						<label for="date">Date</label>
+						<DatePicker
+							selected={this.state.date}
+							onChange={this.onChangeDate}
+						/>
+					</div>
+					<button type="submit" className="btn btn-primary">Submit</button>
+				</form>
+			 </Layout>
         )
     }
 };
--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -1,11 +1,57 @@
 import React from 'react';
+import axios from 'axios';
+import Layout from './Layout';
 
-const CreateUser = () => {
+class CreateUser extends React.Component {
 
-    return(
-        <h2>CreateUser</h2>
-    )
+    //This fields should be equal to the fields in our MongoDB
+    state = {
+        username: '',
+    }
+
+    onChangeUsername = (e) => {
+        this.setState({
+            username: e.target.value,
+        });
+    };
     
+    onFormSubmit = (e) => {
+        e.preventDefault();
+
+        
+        const user = {
+            username: this.state.username
+        }
+
+        //Send the user data to our server
+        axios.post('http:localhost:5000/users/add', user)
+            .then(res => console.log(res.data))
+
+        //Clear the username Field
+        this.setState({
+            username: ''
+        })
+    }
+	
+    render() {
+        return(
+            <Layout>
+                <h3>Create a New User</h3>
+                <form onSubmit ={this.onFormSubmit}>
+                    <div className="form-group">
+                        <label for="description">Description</label>
+                        <input 
+                            type="text"
+                            className="form-control"
+                            id="user"
+                            value={this.state.user}
+                            onChange={this.onChangeUsername} />
+                    </div>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
+             </Layout>
+        )
+    }
 };
 
 export default CreateUser;
